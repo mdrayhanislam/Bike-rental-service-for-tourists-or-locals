f08d24eb5d46ca109984fe2cbde2a5cdf5c727d9
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2024-10-09 12:19:15 +0600

    user interface add

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,12 @@
+import { USER_Role, USER_STATUS } from './user.constants';
+
+export type TUser = {
+  name: string;
+  email: string;
+  password: string;
+  phone: number;
+  address: string;
+  role: keyof typeof USER_Role;
+  status: keyof typeof USER_STATUS;
+  passwordChangedAt?: Date;
+};
